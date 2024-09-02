f6e29d6e6cb36142836db684d2440da9778ada33
HAUNGTOFU <2672916471@qq.com>
2024-09-03 07:56:04 +0800

    Create some

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 with open("README", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name='TimeForecast',
-    version='1.0',
+    name='Timeforecast',
+    version='0.1',
     description='model for time forecast',
     author='huangtofu',
     author_email='2672916471@qq.com',
