8117fcdad617c6ebb1a2ee72dabc36b0a5c102fb
HAUNGTOFU <2672916471@qq.com>
2024-09-02 13:57:03 +0800

    Create some

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+with open("README", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='TimeForecast',
+    version='1.0',
+    description='model for time forecast',
+    author='huangtofu',
+    author_email='2672916471@qq.com',
+    url='https://github.com/HAUNGTOFU/TimeForecast',
+    zip_safe=False,
+    packages=setuptools.find_packages())
+pypi - AgEIcHlwaS5vcmcCJGFkODcxODRjLTRlNGUtNGIwMy04YzdmLWY1NjUwOTQyNTlmZQACKlszLCJjNzFiYzM3My0xNWUwLTQwMmEtYWFlOS03MDg1YzRlYTIyY2UiXQAABiBvRB5vYWdQD5iM4L5Nays3TC0lA7sCMURYqHd8fYaOHA
